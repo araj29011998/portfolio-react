83e65c5ff20f0884045284f2e6b95229e050be35
Abhishek <abhishekrajpermani29011998@gmail.com>
2021-07-03 17:37:18 +0530

    final changes

---
+++ src/data.js
@@ -0,0 +1,63 @@
+export const testimonials = [
+  {
+    quote:
+      "Worked on this project at Samsung Bangalore in Network Access and Core R&D team on Long Call Timer Analysis. With the help of time series model,we predicted the Long Call Duration Value for given dataset and user percentage satisfaction.",
+    image: "./srib.png",
+    name: "Long Call Timer Analysis",
+    company: "Samsung bangalore",
+  },
+  {
+    quote:
+      "This automation bot submits the solution of any problem on Codechef ( Coding Platform ) .Bot automatically performs tasks like - login ,copy-pasting the code and submitting the code. Tech Stack used- Selenium and Python",
+    image: "./codechef.png",
+    name: "Automation Bot",
+    company: "Codechef",
+  },
+];
+
+export const skills = [
+  "C++",
+  "Python",
+  "JavaScript",
+  "React.js",
+  "Machine Learning",
+  "Node.js",
+  "Operations",
+  "Team Management",
+];
+
+export const projects = [
+  {
+    title: "Software Engineer",
+    subtitle: "Jaguar Land Rover",
+    description:
+      "Working in Vehicle Engineering team July,2021- Present",
+    image: "./project-1.jpg",
+    link: "https://www.jaguarlandrover.com/",
+  },
+  {
+    title: "Computer Science",
+    subtitle: "IIT Dhanbad",
+    description:
+      "Completed Bachelor of Technology 2017-21",
+    image: "./project-2.png",
+    link: "https://iitism.ac.in/",
+  },
+  {
+    title: "Operation Manager",
+    subtitle: "Coding Ninjas",
+    description:
+      "Key roles include-Operation,Outreach Manager,Content and Instructor Feb,2021 - July,2021 ",
+    image: "./project-4.png",
+    link: "https://www.codingninjas.com/",
+  },
+  {
+    title: "Software Development Engineer",
+    subtitle: "Samsung",
+    description:
+      "Interned with Network Access and Core R&D Team on long call timers May,2020-July,2020",
+    image: "./project-3.jfif",
+    link: "https://research.samsung.com/sri-b",
+  },
+  
+];
